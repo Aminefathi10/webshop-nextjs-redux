c3c8f1b83bb6ada9bcdb1df6aa5aecc6e61d33d2
Aminefathi10 <aminefathi268@gmail.com>
2024-10-03 14:17:58 +0100

    resolved merge conflicts

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-<<<<<<< HEAD
 import fs from 'fs';
 import path from 'path';
 
@@ -7,15 +6,8 @@ import path from 'path';
 export async function GET(_req, { params }) {
       const dataPath = path.join(process.cwd(), '_data', 'products.json');
       const fileData = fs.readFileSync(dataPath, 'utf8');
-=======
-import { promises as fs } from 'fs';
 
 
-export async function GET(_req, { params }) {
-
-   try {
-      const fileData = await fs.readFileSync('/app/_data/products.json', 'utf8');
->>>>>>> e7df8eb6da9fa0d07089fa7f624cfc936bdbf196
       const jsonData = JSON.parse(fileData).find(item => item.id === parseInt(params.id));
 
       if(jsonData){
@@ -23,10 +15,4 @@ export async function GET(_req, { params }) {
       } else {
       return NextResponse.json({ error: "Item not found!" }, { status: 404 });
       }
-
-<<<<<<< HEAD
 }
-=======
-   
-}
->>>>>>> e7df8eb6da9fa0d07089fa7f624cfc936bdbf196
